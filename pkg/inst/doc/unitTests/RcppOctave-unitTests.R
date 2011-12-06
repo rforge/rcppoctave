32e2343c28266b1bddabbc2291069d77299fee70
renaud <renaud@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-06 11:09:50 +0000

    * add a  vignette for the package * add a vignette for unit tests * add Makefile for vignettes, based on the makefile for RcppGSL. It is improved to be generic and does not require fake vignettes (it generates them on the fly)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcppoctave@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inst/doc/unitTests/RcppOctave-unitTests.R
@@ -0,0 +1,34 @@
+
+pkg <- "RcppOctave"
+
+# load this package
+require( pkg, character.only = TRUE )
+
+## load RUnit
+runit <- "RUnit" ; require( runit, character.only = TRUE )
+if( file.exists( "unitTests-results" ) ){ unlink("unitTests-results", recursive = TRUE ) }
+dir.create( "unitTests-results" )
+
+path <- system.file("unitTests", package = pkg)
+testSuite <- defineTestSuite(name=paste(pkg, "unit testing"), dirs = path
+							, rngKind = "default",
+							, rngNormalKind = "default")
+tests <- runTestSuite(testSuite)
+printHTMLProtocol(tests, fileName= sprintf( "unitTests-results/%s-unitTests.html" , pkg ) )
+printTextProtocol(tests, fileName= sprintf( "unitTests-results/%s-unitTests.txt"  , pkg ) )
+
+# check for errors
+err <- getErrors(tests)
+if( err$nFail > 0) {
+	stop(sprintf( "unit test problems: %d failures", err$nFail))
+}
+if( err$nErr > 0) {
+	stop( sprintf( "unit test problems: %d errors", err$nErr) )
+}
+
+# copy result in tmp directory if possible
+if( file.exists( "/tmp" ) ){
+	file.copy( sprintf( "unitTests-results/%s-unitTests.txt" , pkg ) , "/tmp", overwrite = TRUE )
+	file.copy( sprintf( "unitTests-results/%s-unitTests.html", pkg ) , "/tmp", overwrite = TRUE )
+}
+
