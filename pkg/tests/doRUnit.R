87bff1555cce4d67b6de13c4ec6c8eac29e0426d
renaud <renaud@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-19 17:24:11 +0000

    use pkgmaker makefile and unit test vignette features
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcppoctave@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/doRUnit.R
+++ pkg/tests/doRUnit.R
@@ -4,30 +4,4 @@
 # Creation: 26 Oct 2011
 ###############################################################################
 
-
-#### doRUnit.R --- Run RUnit tests
-####------------------------------------------------------------------------
-
-### borrowed from package Rcpp
-### http://cran.r-project.org/web/packages/Rcpp/index.html
-###
-### Adapted to the package RcppOctave
-if(require("RUnit", quietly = TRUE)) {
-	pkg <- "RcppOctave"
-	
-	require( pkg, character.only=TRUE)
-	
-	path <- system.file("unitTests", package = pkg)
-	
-	stopifnot(file.exists(path), file.info(path.expand(path))$isdir)
-	
-	# without this, we get unit test failures
-	Sys.setenv( R_TESTS = "" )
-	
-	RcppOctave.unit.test.output.dir <- getwd()
-	
-	source(file.path(path, "runTests.R"), echo = TRUE)
-	
-} else {
-	print( "package RUnit not available, cannot run unit tests" )
-}
+pkgmaker::utest('package:RcppOctave', quiet=FALSE)
